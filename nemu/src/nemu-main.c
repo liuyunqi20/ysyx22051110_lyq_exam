a4b9265387663bf9dcbf005b5be6131a79de02aa
tracer-ics2022 <tracer@njuics.org>
2023-03-08 14:52:33 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:52:33 up 26 min,  1 user,  load average: 0.28, 0.24, 0.18

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
   //engine_start();
   /* Test Expr */
   word_t expr(char *e, bool *success);
-  FILE *fp = fopen("~/ics2022/nemu/tools/gen_expr/input", "r");
+  FILE *fp = fopen("/home/llyyqq/ics2022/nemu/input", "r");
   assert(fp != NULL);
   bool flag;
   char buf[200];
