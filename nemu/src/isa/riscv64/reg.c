f1cf60766bbc94bc0fafeec1d9d8857fa97fa3e8
tracer-ics2022 <tracer@njuics.org>
2023-03-08 22:20:36 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:20:36 up  2:12,  1 user,  load average: 0.17, 0.18, 0.18

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -38,8 +38,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   }
   for(int i = 1; i < 32; ++i){
     if(!strcmp(s+1, regs[i])){
-      printf("hit reg %s\n", regs[i]);
-      *success = false;
+      *success = true;
       return cpu.gpr[i];
     }
   }
