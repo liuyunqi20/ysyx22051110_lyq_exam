841a5755d722af30332aa68ec3cbecdac107c56f
tracer-ics2022 <tracer@njuics.org>
2023-03-12 11:06:13 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:06:13 up 7 min,  1 user,  load average: 0.08, 0.57, 0.44

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -63,7 +63,7 @@ static int decode_exec(Decode *s) {
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   decode_operand(s, &dest, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
-  Log("%s: %lx => (%lx)", ""#name, SEXT(src2, 32), src1 + imm); \
+  Log("%s: pc=%lx", ""#name, s->pc); \
 }
 
   INSTPAT_START();
