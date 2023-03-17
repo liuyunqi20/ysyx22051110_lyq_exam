7459db4a673de02a51cb8f37a38b1ecc050cb8c5
tracer-ics2022 <tracer@njuics.org>
2023-03-17 20:36:45 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:45 up  1:25,  1 user,  load average: 0.17, 0.17, 0.18

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -25,7 +25,7 @@ $(BINARY): compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS += --elf=$(ELF_FILE)
+override ARGS += -e $(ELF_FILE)
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
