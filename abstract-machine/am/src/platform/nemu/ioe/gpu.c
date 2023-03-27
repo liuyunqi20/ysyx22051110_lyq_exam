bdcb7477025dc321ccc6e5157d20fa1fd5a2ad54
tracer-ics2022 <tracer@njuics.org>
2023-03-27 16:22:45 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:22:45 up 44 min,  1 user,  load average: 0.22, 0.18, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+int    printf    (const char *format, ...);
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
@@ -8,6 +8,7 @@ void __am_gpu_init() {
   uint32_t vgactl_0 = inl(VGACTL_ADDR);
   int w = (int)(vgactl_0 >> 16) ; 
   int h = (int)(vgactl_0 & 0xffff);
+  printf("w: %d h: %d\n", w,h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -22,7 +23,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  /*uint32_t * cur_pixel = ctl->pixels;
+  uint32_t * cur_pixel = ctl->pixels;
   int fb_w = (int)(inl(VGACTL_ADDR) >> 16); 
   uint32_t *fb = ((uint32_t *)FB_ADDR) + ctl->y * fb_w + ctl->x;
   for(int i = 0; i < ctl->h; ++i){
@@ -31,7 +32,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       cur_pixel++;
     }
     fb += fb_w;
-  }*/
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
