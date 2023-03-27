5d5fed3216edb49d357148b17b15c6dbb829b3ef
tracer-ics2022 <tracer@njuics.org>
2023-03-27 18:27:13 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:27:13 up 13 min,  1 user,  load average: 0.01, 0.26, 0.33

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef CONFIG_DTRACE
+  printf("%s read 0x%lx at offset 0x%x\n", map->name, ret, offset);
+#endif
   return ret;
 }
 
@@ -67,4 +70,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+#ifdef CONFIG_DTRACE
+  printf("%s write 0x%lx at offset 0x%x\n", map->name, data, offset);
+#endif
 }
