816c269b8347445fbca75e324df7c44f417b7f46
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-28 19:38:52 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:38:52 up 36 min,  1 user,  load average: 0.02, 0.01, 0.00

--- npc/sim/cpp/src/memory/memory.cpp
+++ npc/sim/cpp/src/memory/memory.cpp
@@ -89,7 +89,11 @@ extern "C" void cpu_dmem_write(svBit en, svBit wr, long long waddr, long long wd
     //printf("waddr: %llx wdata: %llx\n", waddr, wdata);
     // ---------------- mmio ---------------- 
       if((uint64_t)waddr == (uint64_t)SERIAL_PORT) { putchar((uint8_t)wdata); return;}
-      if((uint64_t)waddr == (uint64_t)VGACTL_ADDR) { if(wmask == 0xf0) vga_ctl |= (wdata & 0xffffffff00000000); }
+      if((uint64_t)waddr == (uint64_t)VGACTL_ADDR) { 
+        if(wmask == 0xf0) 
+          vga_ctl |= (wdata & 0xffffffff00000000); 
+        return;
+      }
       if((uint64_t)waddr >= (uint64_t)FB_ADDR)     { 
         uint64_t offset = (uint64_t)waddr - FB_ADDR;
         if(wmask == 0xf0)
