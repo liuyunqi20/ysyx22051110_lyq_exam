12be6628b85fe7d31d940fb0724e4410281f26e8
tracer-ics2022 <tracer@njuics.org>
2023-03-31 18:36:19 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:19 up  1:00,  1 user,  load average: 0.10, 0.07, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,31 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#ifdef __ISA_AM_NATIVE__
+#define EXPECT_TYPE EM_X86_64
+#elif __ISA_RISCV32__
+#define EXPECT_TYPE EM_RISCV
+#elif __ISA_RISCV64__
+#define EXPECT_TYPE EM_RISCV
+#endif
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)(&(ehdr.e_ident)) == 0x464c457f);
+  assert(ehdr.e_machine == EXPECT_TYPE);
+  Elf_Phdr phdr;
+  for(int i = 0; i < ehdr.e_phnum; ++i){
+    ramdisk_read(&phdr, ehdr.e_phoff + (i * ehdr.e_phentsize), ehdr.e_phentsize);
+    if(phdr.p_type == PT_LOAD){
+      uintptr_t p_vaddr = phdr.p_vaddr;
+      ramdisk_read((void *)p_vaddr, phdr.p_offset, phdr.p_filesz);
+      memset((void *)(p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
