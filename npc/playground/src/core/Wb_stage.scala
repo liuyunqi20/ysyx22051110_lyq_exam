2237ea5f8680296617b2646e1d9d0f511fb14efe
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-31 11:31:31 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:31:31 up 49 min,  1 user,  load average: 0.05, 0.09, 0.09

--- npc/playground/src/core/Wb_stage.scala
+++ npc/playground/src/core/Wb_stage.scala
@@ -14,9 +14,9 @@ class Wb_stage(w: Int) extends Module{
     })
     // ------------------ intrrupt/exception ------------------ 
     io.exc_br.exc_br     := (io.mem2wb.exc_type.orR === 1.U) || (io.csr_exc.intr_t)
-    io.exc_br.exc_target := Mux1H(Seq(
-        /* ecall */ io.mem2wb.exc_type(0) -> (io.csr_out.mtvec),
-        /* mret  */ io.mem2wb.exc_type(1) -> (io.csr_out.mepc + 4.U(w.W)),
+    io.exc_br.exc_target := MuxCase(0.U(w.W), Array(
+        ( (io.mem2wb.exc_type(0) === 1.U) || io.csr_exc.intr_t ) -> (io.csr_out.mtvec),    /* trap entry */
+        (  io.mem2wb.exc_type(1) === 1.U                       ) -> (io.csr_out.mepc + 4.U(w.W)),  /*trap return */
     ))
     val exc_code    = MuxLookup(io.mem2wb.exc_type, 0.U(w.W), Seq(
         /* ecall  */ io.mem2wb.exc_type(0) -> (11.U(w.W)),
