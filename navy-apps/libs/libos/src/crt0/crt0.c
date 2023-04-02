be2a9270c65cb70875c58995ffd02d71b562689b
tracer-ics2022 <tracer@njuics.org>
2023-04-02 20:44:33 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:33 up 18 min,  1 user,  load average: 0.09, 0.08, 0.09

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  exit(main(0, empty, empty));
+  int ret = main(0, empty, empty);
+  printf("main returned %d\n", ret);
+  exit(ret);
   assert(0);
 }
