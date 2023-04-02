fc0bf6c44f7963bcbd7d378f4cbdf2201aaed60b
tracer-ics2022 <tracer@njuics.org>
2023-04-02 16:25:32 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:32 up 50 min,  1 user,  load average: 0.03, 0.04, 0.03

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,12 +6,17 @@
 
 int main(){
     int count = 0;
+    printf("timer test begin: (0.5s)\n");
     uint32_t cur_tick = NDL_GetTicks(); 
+    printf("%d\n", NDL_GetTicks());
+    for(int i =0 ; i < 10000; i++) ++i;
+    printf("%d\n" , NDL_GetTicks())
     while(count < 20){
         while((NDL_GetTicks() - cur_tick) / 6000 < 500000) {;}
         printf("What a nice day!\n");
         count++;
         cur_tick = NDL_GetTicks();
     }
+    printf("timer test end\n");
     return 0;
 }
