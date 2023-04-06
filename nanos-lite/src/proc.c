f22cebdc7532f5dd7ae725cfb314b66fe0557855
tracer-ics2022 <tracer@njuics.org>
2023-04-06 22:47:00 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:47:00 up  1:31,  1 user,  load average: 0.54, 0.66, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/171860543");
+  naive_uload(NULL, "/bin/171860575");
 }
 
 Context* schedule(Context *prev) {
