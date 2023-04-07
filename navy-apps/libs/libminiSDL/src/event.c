51f23b464b1674e89e5450e0305a1bf59880fac6
tracer-ics2022 <tracer@njuics.org>
2023-04-07 21:58:58 +0800

    >  compile NEMU 22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:58 up  1:11,  1 user,  load average: 0.19, 0.10, 0.04

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #define keyname(k) #k,
 
@@ -9,6 +10,20 @@ static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+int str2keysym(char * buf){
+  char * temp = buf;
+  buf[strlen(buf) - 1] = '\0';
+  temp += 2;
+  while((*temp) == ' ') temp++;
+  char keystr[16];
+  strncpy(keystr, temp, sizeof(keystr));
+  for(int i = 0; i < (sizeof(keyname)/sizeof(char *)); ++i)
+    if(!strcmp(keyname[i], keystr)){
+      return i;
+    }
+  assert(0);
+}
 
 int SDL_PushEvent(SDL_Event *ev) {
   printf("SDL_PushEvent not implemented\n");
@@ -21,32 +36,14 @@ int SDL_PollEvent(SDL_Event *ev) {
     return 0;
   char * temp = NULL;
   if(ev == NULL) return 1;
-  if((temp = strstr(buf, "kd")) && (temp == buf)){
+  if(buf[0] == 'k' && buf[1] == 'd'){
     ev->type = SDL_KEYDOWN;
     ev->key.type = SDL_KEYDOWN;
-    buf[strlen(buf) - 1] = '\0';
-    temp += 2;
-    while((*temp) == ' ') temp++;
-    char keystr[16];
-    strncpy(keystr, temp, sizeof(keystr));
-    for(int i = 0; i < (sizeof(keyname)/sizeof(char *)); ++i)
-      if(!strcmp(keyname[i], keystr)){
-        ev->key.keysym.sym = i;
-        break;
-      }
-  }else if((temp = strstr(buf, "ku")) && (temp == buf)){
+    ev->key.keysym.sym = str2keysym(buf);
+  }else if(buf[0] == 'k' && buf[0] == 'u'){
     ev->type = SDL_KEYUP;
     ev->key.type = SDL_KEYUP;
-    buf[strlen(buf) - 1] = '\0';
-    temp += 2;
-    while((*temp) == ' ') temp++;
-    char keystr[16];
-    strncpy(keystr, temp, sizeof(keystr));
-    for(int i = 0; i < (sizeof(keyname)/sizeof(char *)); ++i)
-      if(!strcmp(keyname[i], keystr)){
-        ev->key.keysym.sym = i;
-        break;
-      }
+    ev->key.keysym.sym = str2keysym(buf);
   }else
     ev->type = SDL_USEREVENT;
   return 1;
@@ -56,32 +53,14 @@ int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
   while(!NDL_PollEvent(buf, sizeof(buf))){;}
   char * temp = NULL;
-  if((temp = strstr(buf, "kd")) && (temp == buf)){
+  if(buf[0] == 'k' && buf[1] == 'd'){
     event->type = SDL_KEYDOWN;
     event->key.type = SDL_KEYDOWN;
-    buf[strlen(buf) - 1] = '\0';
-    temp += 2;
-    while((*temp) == ' ') temp++;
-    char keystr[16];
-    strncpy(keystr, temp, sizeof(keystr));
-    for(int i = 0; i < (sizeof(keyname)/sizeof(char *)); ++i)
-      if(!strcmp(keyname[i], keystr)){
-        event->key.keysym.sym = i;
-        break;
-      }
-  }else if((temp = strstr(buf, "ku")) && (temp == buf)){
+    event->key.keysym.sym = str2keysym(buf);
+  }else if(buf[0] == 'k' && buf[0] == 'u'){
     event->type = SDL_KEYUP;
     event->key.type = SDL_KEYUP;
-    buf[strlen(buf) - 1] = '\0';
-    temp += 2;
-    while((*temp) == ' ') temp++;
-    char keystr[16];
-    strncpy(keystr, temp, sizeof(keystr));
-    for(int i = 0; i < (sizeof(keyname)/sizeof(char *)); ++i)
-      if(!strcmp(keyname[i], keystr)){
-        event->key.keysym.sym = i;
-        break;
-      }
+    event->key.keysym.sym = str2keysym(buf);
   }else
     event->type = SDL_USEREVENT;
   return 1;
