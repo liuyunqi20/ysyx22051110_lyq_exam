904f83e5138692e85b19f59af682178d5b12783e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 20:34:08 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:08 up  1:10,  1 user,  load average: 0.00, 0.01, 0.03

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -104,7 +104,7 @@ void set_npc_state(int state, uint64_t pc, uint32_t ret){
 void execute_once(){
     //printf("start %lx\n", cpu_pc);
     uint64_t old_pc = cpu_pc;
-    while(cpu_pc != old_cpu){
+    while(cpu_pc != old_pc){
         // ----------------- NEG ----------------- 
         contextp->timeInc(1);
         SimTop->clock = !SimTop->clock;
