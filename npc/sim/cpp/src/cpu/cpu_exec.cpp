1e74475ca5ca716c5be7be48b564dab4addb9582
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 20:35:29 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:29 up  1:12,  1 user,  load average: 0.11, 0.06, 0.05

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -129,6 +129,7 @@ void execute_once(){
         //update current pc
         cpu_pc = SimTop->io_core_debug_debug_pc;
         VSimTop::catch_ebreak(&ebreak_f);
+        printf("cur_pc: %lx old_pc: %lx\n", cpu_pc, old_pc);
     }
 }
 
