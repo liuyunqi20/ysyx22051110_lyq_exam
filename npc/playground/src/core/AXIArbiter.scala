04c74379070f0ab58c73053e25c38f1b2cc128e2
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 21:43:19 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:19 up  2:30,  1 user,  load average: 0.19, 0.24, 0.16

--- npc/playground/src/core/AXIArbiter.scala
+++ npc/playground/src/core/AXIArbiter.scala
@@ -28,15 +28,16 @@ class AXIArbiter(w: Int) extends Module with HasArbiterConst{
     })
     // --------------------- read arbiter  ------------------------ 
     //generate arbiter input vector
-    val arbiter_src = IO(new Bundle{ val in = Output(Vec(2, Decoupled(Bits(w.W)))) })
-    // arbiter_src(0).valid := io.rd_IFU.en  
-    // arbiter_src(0).valid := io.rd_MSU.en
-    // arbiter_src(1).bits  := io.rd_IFU.addr
-    // arbiter_src(1).bits  := io.rd_IFU.addr
-    arbiter_src.in(0).valid := io.rd_IFU.en  
-    arbiter_src.in(1).valid := io.rd_MSU.en
-    arbiter_src.in(0).bits := io.rd_IFU.addr
-    arbiter_src.in(1).bits := io.rd_IFU.addr
+    //val arbiter_src = IO(new Bundle{ val in = Output(Vec(2, Decoupled(Bits(w.W)))) })
+    val arbiter_src = Vec(2, Decoupled(Bits(w.W)))
+    arbiter_src(0).valid := io.rd_IFU.en  
+    arbiter_src(1).valid := io.rd_MSU.en
+    arbiter_src(0).bits  := io.rd_IFU.addr
+    arbiter_src(1).bits  := io.rd_MSU.addr
+    // arbiter_src.in(0).valid := io.rd_IFU.en  
+    // arbiter_src.in(1).valid := io.rd_MSU.en
+    // arbiter_src.in(0).bits  := io.rd_IFU.addr
+    // arbiter_src.in(1).bits  := io.rd_MSU.addr
     //choose signal to read
     val arbiter_rd = Module(new Arbiter(UInt(w.W), 2))
     arbiter_rd.io.in <> arbiter_src
