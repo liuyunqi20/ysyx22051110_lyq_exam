2b090385d76a0e114e6958c3ac8ebc79d9d2aa48
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 22:36:15 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:15 up  3:23,  1 user,  load average: 0.20, 0.18, 0.13

---
+++ npc/sim/obj_dir/VSimTop__Dpi.cpp
@@ -0,0 +1,24 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Implementation of DPI export functions.
+//
+// Verilator compiles this file in when DPI functions are used.
+// If you have multiple Verilated designs with the same DPI exported
+// function names, you will get multiple definition link errors from here.
+// This is an unfortunate result of the DPI specification.
+// To solve this, either
+//    1. Call VSimTop::{export_function} instead,
+//       and do not even bother to compile this file
+// or 2. Compile all __Dpi.cpp files in the same compiler run,
+//       and #ifdefs already inserted here will sort everything out.
+
+#include "VSimTop__Dpi.h"
+#include "VSimTop.h"
+
+#ifndef VL_DPIDECL_catch_ebreak_
+#define VL_DPIDECL_catch_ebreak_
+void catch_ebreak(int* i) {
+    // DPI export at ../build/SimTop.v:1561:9
+    return VSimTop::catch_ebreak(i);
+}
+#endif
+
