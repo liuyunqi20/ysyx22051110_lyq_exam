7ff1fe4aff995143fbea00c1fa0d83316f16d789
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-14 22:36:16 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:16 up  1:22,  1 user,  load average: 0.16, 0.19, 0.24

--- npc/playground/src/core/AXIArbiter.scala
+++ npc/playground/src/core/AXIArbiter.scala
@@ -34,16 +34,15 @@ class AXIArbiter(w: Int, nr_src: Int) extends Module with HasArbiterConst{
     for( i <- 0 until nr_src){
         io.in(i).rd.valid := io.out.rd.valid && (rd_chosen === i.U)
         io.in(i).rd.bits  <> io.out.rd.bits
-        io.out.rd.ready   := io.in(i).rd.ready
     }
-
+    io.out.rd.ready   := io.in(rd_chosen).rd.ready
 
     /*
         Choose one write port and interact with AXI module.
     */
     val arbiter_wt = Module(new Arbiter(new AXI4LiteAW(w), nr_src))
     val wt_chosen  = RegInit(0.U(log2Ceil(nr_src).W))  //record which port for current issue
-    when(io.out.ar.fire){
+    when(io.out.aw.fire){
         wt_chosen := arbiter_wt.io.chosen
     }
     // --------------------------- write arbiter in --------------------------- 
@@ -59,12 +58,11 @@ class AXIArbiter(w: Int, nr_src: Int) extends Module with HasArbiterConst{
     io.out.aw.bits            <> arbiter_wt.io.out.bits
     // --------------------------- write data&response --------------------------- 
     for( i <- 0 until nr_src){
-        io.in(i).wt.ready := io.out.wt.ready && (rd_chosen === i.U)
+        io.in(i).wt.ready := io.out.wt.ready && (wt_chosen === i.U)
         io.in(i).wt.bits  <> io.out.wt.bits
-        io.out.wt.valid   := io.in(i).wt.valid && (wt_chosen === i.U)
-        io.in(i).b.valid  := io.out.b.valid && (rd_chosen === i.U)
+        io.in(i).b.valid  := io.out.b.valid && (wt_chosen === i.U)
         io.in(i).b.bits   <> io.out.b.bits
-        io.out.b.ready    := io.in(i).b.ready && (wt_chosen === i.U)
     }
-
+    io.out.wt.valid   := io.in(wt_chosen).wt.valid
+    io.out.b.ready    := io.in(wt_chosen).b.ready
 }
