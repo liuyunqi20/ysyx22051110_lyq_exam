0da1518b26d52ecc0674294da12d300c26c30f21
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 21:47:05 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:05 up  1:34,  1 user,  load average: 0.29, 0.24, 0.21

--- npc/playground/src/core/cache/CacheTop.scala
+++ npc/playground/src/core/cache/CacheTop.scala
@@ -60,7 +60,7 @@ class CacheStage2(config: CacheConfig) extends Module{
     val s2_ready_go        = true.B
     val s2_valid           = RegInit(0.U(1.W))
     val io.s1_to_s2.ready := (s2_valid || (s2_ready_go && io.s2_to_s3.ready))
-    val io.s2_to_s3.valid := (s2_valid && s2_ready_go)
+    val io.s2_to_s3.valid = (s2_valid && s2_ready_go)
     when(io.s1_to_s2.ready){
         s2_valid := io.s1_to_s2.valid
     }
