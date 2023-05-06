fe68f198eb4b06d6b21286050a8a61c789759353
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 22:07:14 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:07:14 up  2:30,  1 user,  load average: 0.21, 0.27, 0.24

--- npc/playground/src/core/cache/CacheTop.scala
+++ npc/playground/src/core/cache/CacheTop.scala
@@ -65,11 +65,10 @@ class CacheTop(w: Int, tag_w: Int, nr_lines: Int, nr_ways: Int, block_size: Int)
     val cache_data_addr_w = 6
     val cache_data = Seq.fill(nr_ways){ Module(new CacheDataRam()).io }
     //val meta_rd  = RegInit(Vec(nr_ways, 0.U.asTypeOf(new CacheMetaBundle(config.tag_width))))
-    val meta_rd  = RegInit(VecInit( Seq.fill(nr_ways) { 0.U.asTypeOf(new CacheMetaBundle(config.tag_width)) } ))
-    val cache_meta = RegInit(VecInit(Seq.fill(nr_ways){
-                                    Seq.fill(nr_lines){ 0.U.asTypeOf(new CacheMetaBundle(config.tag_width)) }
-                                }
-                            ))
+    val meta_rd    = RegInit(VecInit( Seq.fill(nr_ways) { 0.U.asTypeOf(new CacheMetaBundle(config.tag_width)) } ))
+    val cache_meta = Seq.fill(nr_ways) {
+        RegInit(VecInit(Seq.fill(nr_lines){ 0.U.asTypeOf(new CacheMetaBundle(config.tag_width)) } ))
+    }
     //val cache_meta = Seq.fill(nr_ways){RegInit()}
     when(stage1.io.rd.en){
         for( i <- 0 until nr_ways){
