e601ab23c9d78829f3819f3b06c1adc612d21882
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-08 22:18:53 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:18:53 up  3:20,  1 user,  load average: 0.75, 0.45, 0.29

--- npc/playground/src/core/MycpuCoreTop.scala
+++ npc/playground/src/core/MycpuCoreTop.scala
@@ -26,7 +26,7 @@ class MycpuCoreTop(w: Int, nr_mport: Int) extends Module with HasCoreTopConst{
     val my_csr         = Module(new Csr(w))
     val my_axi_bridge0 = Module(new AXIBridge(w, ICache_block_size * 8 / w))
     val my_axi_bridge1 = Module(new AXIBridge(w, DCache_block_size * 8 / w))
-    val my_mmc         = Module(new MemoryController(w, DCache_block_size * 8 / w))
+    //val my_mmc         = Module(new MemoryController(w, DCache_block_size * 8 / w))
     //ICache: 
     val my_icache      = Module(new CacheTop(w, Cache_tag_width, ICache_nr_lines, 
                                             ICache_nr_ways, ICache_block_size))
