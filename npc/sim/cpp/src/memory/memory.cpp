84b672b01f509dcc143bae5bf7cd3817d5b90c4f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 23:47:55 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:47:55 up 24 min,  1 user,  load average: 0.23, 0.33, 0.29

--- npc/sim/cpp/src/memory/memory.cpp
+++ npc/sim/cpp/src/memory/memory.cpp
@@ -89,9 +89,9 @@ extern "C" void cpu_dmem_read(svBit en, svBit wr, long long raddr, long long * r
         return;
       }
     // ---------------- memory ---------------- 
-    if(!(raddr >= MBASE && raddr < (MBASE + MSIZE))){
-       //printf("pc: %lx r: %lx\n", cpu_pc, (uint64_t)raddr); wave_end(); }
-        *rdata = 0; return;}
+    // if(!(raddr >= MBASE && raddr < (MBASE + MSIZE))){
+    //    //printf("pc: %lx r: %lx\n", cpu_pc, (uint64_t)raddr); wave_end(); }
+    //     *rdata = 0; return;}
     assert(raddr >= MBASE && raddr < (MBASE + MSIZE));
     uint64_t ret;
     ret = (long long)vaddr_read(raddr, 8);
@@ -136,10 +136,9 @@ extern "C" void cpu_dmem_write(svBit en, svBit wr, long long waddr, long long wd
         return;
       }
     // ---------------- memory ---------------- 
-    // if(!(waddr >= MBASE && waddr < (MBASE + MSIZE))){
-    //   wave_end();
-    //   printf("pc: %lx w: %lx %lx\n", cpu_pc, (uint64_t)waddr, (uint64_t)wdata);
-    // }
+    if(!(waddr >= MBASE && waddr < (MBASE + MSIZE))){
+      printf("pc: %lx w: %lx %lx\n", cpu_pc, (uint64_t)waddr, (uint64_t)wdata); wave_end();
+    }
     assert(waddr >= MBASE && waddr < (MBASE + MSIZE));
     uint64_t temp_data = wdata;
     int len = 0;
