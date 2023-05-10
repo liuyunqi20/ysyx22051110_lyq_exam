29abebbcd7fa238f525220fc194392dd6bf4360b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 21:04:31 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:31 up  2:15,  1 user,  load average: 0.43, 0.36, 0.29

--- npc/sim/obj_dir/VSimTop__Dpi.h
+++ npc/sim/obj_dir/VSimTop__Dpi.h
@@ -13,17 +13,17 @@ extern "C" {
 
 
     // DPI EXPORTS
-    // DPI export at ../build/SimTop.v:3753:9
+    // DPI export at ../build/SimTop.v:3763:9
     extern void catch_ebreak(int* i);
 
     // DPI IMPORTS
-    // DPI import at ../build/SimTop.v:3795:37
+    // DPI import at ../build/SimTop.v:3805:37
     extern void cpu_dmem_read(svBit en, svBit wr, long long raddr, long long* rdata);
-    // DPI import at ../build/SimTop.v:3813:37
+    // DPI import at ../build/SimTop.v:3823:37
     extern void cpu_dmem_write(svBit en, svBit wr, long long waddr, long long wdata, char wmask);
-    // DPI import at ../build/SimTop.v:3780:33
+    // DPI import at ../build/SimTop.v:3790:33
     extern void set_gpr_ptr(const svOpenArrayHandle a);
-    // DPI import at ../build/SimTop.v:3750:33
+    // DPI import at ../build/SimTop.v:3760:33
     extern void set_inst_ptr(const svOpenArrayHandle a);
 
 #ifdef __cplusplus
