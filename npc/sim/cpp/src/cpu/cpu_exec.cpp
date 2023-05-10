fd4bcd52a554721237ba25db461bbd4094f485e9
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 21:32:44 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:32:44 up  2:43,  1 user,  load average: 0.14, 0.17, 0.20

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -88,7 +88,9 @@ void write_itrace(uint64_t pc, uint64_t inst){
     }
     memset(p, ' ', 4);
     p += 4;
+    printf("this1\n");
     disassemble(p, IBUF_SIZE-(p-iringbuf.buf[iringbuf.head]), pc, (uint8_t *)&inst, ilen);
+    printf("this2\n");
     if(iringbuf.head == (IRINGBUF_SIZE - 1))
     iringbuf.head = 0;
     else
@@ -232,9 +234,7 @@ void init_cpu(){
     wave_dump();
     cpu_pc = SimTop->io_core_debug_debug_pc;
     //execute one step to fetch the first instruction
-    printf("before\n");
     execute_once();
-    printf("next\n");
     printf("[npc] cpu init success!\n");
     npc_state.state = NPC_STOP;
 }
