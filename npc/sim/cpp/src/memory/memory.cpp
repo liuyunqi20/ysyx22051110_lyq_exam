146396c2e8c6fea925de6a84e231a2ad7f9e8224
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 21:32:49 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:32:49 up  2:21,  1 user,  load average: 0.14, 0.24, 0.26

--- npc/sim/cpp/src/memory/memory.cpp
+++ npc/sim/cpp/src/memory/memory.cpp
@@ -108,7 +108,7 @@ extern "C" void cpu_dmem_write(svBit en, svBit wr, long long waddr, long long wd
   if(en && wr){
     waddr = waddr & ~0x7;
     if(waddr == 0x8009efe8){
-      printf("pc: %lx 9efe8, w: %lx\n", cpu_pc, waddr, wdata);
+      printf("pc: %lx 9efe8, w: %lx\n", cpu_pc, wdata);
     }
     //printf("waddr: %llx wdata: %llx\n", waddr, wdata);
     // ---------------- mmio ---------------- 
