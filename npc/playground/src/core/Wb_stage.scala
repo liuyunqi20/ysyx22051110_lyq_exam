dfecbdc9d46d4357feb334dce8526619a026e77f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 22:17:21 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:21 up  3:06,  1 user,  load average: 0.27, 0.22, 0.22

--- npc/playground/src/core/Wb_stage.scala
+++ npc/playground/src/core/Wb_stage.scala
@@ -35,6 +35,9 @@ class Wb_stage(w: Int) extends Module{
     io.csr_exc.epc       := io.pc
     io.csr_exc.exc_code  := exc_code
     // ------------------ RF write back ------------------ 
+    val exc_buf     = RegInit(0.B)
+    when(has_trap && ~exc_buf) { exc_buf := 1.B }
+    .elsewhen(io.fs_next_ok) { exc_buf := 0.B }
     io.wb2rf.rf_we := io.mem2wb.gr_we && ~has_trap && io.fs_next_ok
     io.wb2rf.waddr := io.mem2wb.dest
     io.wb2rf.wdata := Mux(io.mem2wb.csr_op.orR === 1.U, io.csr_op.csr_old, io.mem2wb.result)
