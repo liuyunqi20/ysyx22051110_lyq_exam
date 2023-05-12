db16fec4f991e70de03542b0f27bfa1a6a0ed6a8
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 22:54:50 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:54:50 up  3:27,  1 user,  load average: 0.57, 0.48, 0.39

--- npc/playground/src/core/cache/CacheRam.scala
+++ npc/playground/src/core/cache/CacheRam.scala
@@ -81,7 +81,7 @@ class CacheMetaRamV(tag_width: Int) extends BlackBox with HasBlackBoxInline{
         |   input valid        , input dirty , input [22 : 0] tag,
         |   input [5 : 0] addr , input wvalid, input wdirty      ,
         |   input [22 : 0] wtag );
-        |   reg [23 : 0] ram_tag[63 : 0];
+        |   reg [22 : 0] ram_tag[63 : 0];
         |   reg ram_valid[63 : 0];
         |   reg ram_dirty[63 : 0];
         |   reg [23 : 0] rtag; 
