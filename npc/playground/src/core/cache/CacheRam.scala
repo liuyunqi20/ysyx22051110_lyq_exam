a4c0e820f307d5cf6b95dcf4caaf177960271955
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 22:51:22 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:22 up  3:23,  1 user,  load average: 0.28, 0.42, 0.35

--- npc/playground/src/core/cache/CacheRam.scala
+++ npc/playground/src/core/cache/CacheRam.scala
@@ -21,7 +21,7 @@ class CacheDataRam extends BlackBox with HasBlackBoxInline{
         |   input  [127 : 0] D, output [127 : 0] Q    );
         |   wire cen = ~CEN;
         |   wire wen = ~WEN;
-        |   wire bwen = ~BWEN;
+        |   wire [127 : 0] bwen = ~BWEN;
         |   reg [127 : 0] ram[63 : 0];
         |   reg [127 : 0] rdata;
         |   always @(posedge clock) begin
