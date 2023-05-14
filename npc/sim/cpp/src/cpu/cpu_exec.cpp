b0a8cb57962301492753846b829acd28e7b4d45e
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-14 14:19:01 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:01 up  2:49,  1 user,  load average: 0.11, 0.18, 0.23

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -157,7 +157,7 @@ void execute(uint64_t step){
             break;
         }
         //printf("pc: %lx\n", cpu_pc);
-        //if(g_nr_step > 100) return;
+        if(g_nr_step > 300) return;
 #ifdef DEVICE
         device_update();
 #endif
