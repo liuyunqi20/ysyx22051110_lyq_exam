83d8153d7a333a5c75dda331ea80f6f08797e146
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 12:32:55 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:32:55 up 15 min,  1 user,  load average: 0.24, 0.18, 0.15

--- npc/playground/src/core/Ex_stage.scala
+++ npc/playground/src/core/Ex_stage.scala
@@ -84,7 +84,7 @@ class Ex_stage(w: Int) extends Module{
         io.ex2mem.bits.csr_num   := ds_es_r.csr_num
         io.ex2mem.bits.rs1       := ds_es_r.rs1
     // ------------------------ forwarding ------------------------ 
-        io.es_forward.valid            := io.ex2mem.valid && ~(ds_es_r.mem_en && ~ds_es_r.mem_wr) //not load
+        io.es_forward.valid            := io.ex2mem.valid && ~(ds_es_r.mem_en && ~ds_es_r.mem_wr) && ~ds_es_r.csr_op.orR
         io.es_forward.bits.stage_valid := es_valid && io.ex2mem.bits.gr_we
         io.es_forward.bits.dest        := io.ex2mem.bits.dest
         io.es_forward.bits.data        := io.ex2mem.bits.result
