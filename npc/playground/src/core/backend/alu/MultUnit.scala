f12cf0caab45daeca9e718a93e0e52674c3b16b5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-30 22:11:35 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:11:35 up  1:25,  1 user,  load average: 0.13, 0.15, 0.15

--- npc/playground/src/core/backend/alu/MultUnit.scala
+++ npc/playground/src/core/backend/alu/MultUnit.scala
@@ -7,7 +7,7 @@ class MultUnit(w: Int) extends Module{
         val in  = Flipped(Decoupled(new MultUnitInBundle(w)))
         val out = Valid(new MultUnitOutBundle(w)) 
     })
-    val mult_core = Module(new MultShiftAdd(w))
+    val mult_core = Module(new MultBooth2(w))
     io <> mult_core.io
 }
 
@@ -73,7 +73,77 @@ class MultShiftAdd(w: Int) extends Module{
 }
 
 class MultBooth2(w: Int) extends Module{
+    val io = IO(new Bundle{
+        val in  = Flipped(Decoupled(new MultUnitInBundle(w)))
+        val out = Valid(new MultUnitOutBundle(w)) 
+    })
+    val max_cnt  = w/2 + 1
+    val src1_r   = RegInit(0.U((2*w + 4).W))
+    val src2_r   = RegInit(0.U((w + 3).W))
+    val signed_r = RegInit(0.U(2.W)) //b11:sxs b10:sxu b00:uxu
+    val mulw_r   = RegInit(0.B)
+    val res_r    = RegInit(0.U(((2 * w).W)))
+    val cnt      = RegInit(0.U(max_cnt.W))
+    val done     = RegInit(0.B)
+    // ------------------------ counter ------------------------ 
+    val last_step = (cnt(w-1) === 1.U && ~mulw_r) || (cnt(31)  === 1.U && mulw_r)
+    val working   = cnt.orR === 1.U
+    when(io.in.bits.flush || last_step){
+        cnt := 0.U
+    }.elsewhen(io.in.fire){
+        cnt := 1.U
+    }.otherwise{
+        cnt := Cat(cnt(w-2, 0), 0.U(1.W))
+    }
 
+    // ------------------------ input buffer ------------------------ 
+    val src1_double     = Cat(src1_r, 0.U(1.W))
+    val src1_neg        = ~src1_r + 1.U
+    val src1_double_neg = Cat(src1_neg, 0.U(1.W))
+    when(io.in.fire){
+        src1_r   := Cat(Mux(io.in.bits.mul_signed(1), Fill(w, io.in.bits.multiplicand(w + 4)), 0.U((w + 4).W)),
+                        io.in.bits.multiplicand)
+        src2_r   := Cat(Mux(io.in.bits.mul_signed(0), Fill(2, io.in.bits.multiplier(w - 1)), 0.U(2.W)), 
+                        io.in.bits.multiplier, 0.U(1.W))
+        mulw_r   := io.in.bits.mulw
+        signed_r := io.in.bits.mul_signed
+        res_r    := 0.U(w.W)
+    }
+    // ------------------------ booth control ------------------------ 
+    val sel   = MuxLookUp(src2_r(2, 0), 0.U, List(
+        ("b000".U) -> "b00001".U, // 0
+        ("b001".U) -> "b00010".U, // + x
+        ("b010".U) -> "b00010".U, // + x
+        ("b011".U) -> "b00100".U, // + 2x
+        ("b100".U) -> "b01000".U, // - 2x
+        ("b101".U) -> "b10000".U, // - x
+        ("b110".U) -> "b10000".U, // - x
+        ("b111".U) -> "b00001".U, // 0
+    ))
+    val p     = Mux1H(Seq(
+        /* 0   */ sel(0) -> 0.U((2*w + 4).W),
+        /* +x  */ sel(1) -> src1_r,
+        /* +2x */ sel(2) -> src1_double,
+        /* -2x */ sel(3) -> src1_double_neg,
+        /* -x  */ sel(4) -> src1_neg,
+    )) 
+    // ------------------------ add accumulate ------------------------ 
+    when(working) {
+        res_r  := p + res_r
+        src1_r := Cat(src1_r(2*w + 1, 0), 0.U(2.W))
+        src2_r := Cat(Fill(2, src2_r(w + 2)), src2_r(w + 2, 2))
+    }
+    // ------------------------ done  ------------------------ 
+    when(done) { 
+        done := 0.B
+    }.elsewhen(last_step) {
+        done := 1.B
+    }
+
+    io.in.ready  := ~working
+    io.out.valid := done
+    io.out.bits.result_hi := Mux(mulw_r, Fill(w, res_r(31)), res_r(2 * w - 1, w))
+    io.out.bits.result_lo := Cat(Mux(mulw_r, Fill(32, res_r(31)), res_r(w - 1, 32)), res_r(31, 0))
 }
 
 // class MultWallaceTree(w: Int) extends Module{
