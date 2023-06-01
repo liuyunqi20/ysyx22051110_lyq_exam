f037b5fc2b3a838c71133b4fa7d477305cf73fab
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 11:17:23 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:23 up  1:00,  1 user,  load average: 0.16, 0.24, 0.26

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -146,7 +146,7 @@ void execute(uint64_t step){
         g_nr_step++;
 #ifdef DIFFTEST
         if(SimTop->io_core_debug_raise_intr) difftest_raise_intr(INTR_T_NO);
-        difftest_step(SimTop->io_core_debug_debug_pc, SimTop->io_core_debug_debug_nextpc);
+        difftest_step(SimTop->io_core_debug_debug_pc, 0);
 #endif
         if(npc_state.state != NPC_RUNNING) break;
         if(ebreak_f == 1){
