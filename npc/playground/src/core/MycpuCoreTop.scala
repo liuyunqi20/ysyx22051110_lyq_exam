3c3f75b69805b475fa7f828eb90c08b26ed235fd
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-03 11:41:36 +0800

    > generate verilog ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:41:36 up 32 min,  1 user,  load average: 0.21, 0.24, 0.28

--- npc/playground/src/core/MycpuCoreTop.scala
+++ npc/playground/src/core/MycpuCoreTop.scala
@@ -35,14 +35,9 @@ class MycpuCoreTop(w: Int, nr_mport: Int) extends Module with HasCoreTopConst{
     val my_mem         = Module(new Mem_stage(w))
     val my_wb          = Module(new Wb_stage(w))
     val my_csr         = Module(new Csr(w))
-    val my_axi_bridge0 = Module(new AXIBridge(w, ICache_block_size * 8 / w))
-    val my_axi_bridge1 = Module(new AXIBridge(w, DCache_block_size * 8 / w))
-    val my_mmc         = Module(new MemoryController(w, DCache_block_size * 8 / w))
-    //ICache: 
-    val my_icache      = Module(new CacheTop(w, Cache_tag_width, ICache_nr_lines, 
-                                            ICache_nr_ways, ICache_block_size))
-    val my_dcache      = Module(new CacheTop(w, Cache_tag_width, DCache_nr_lines,
-                                            DCache_nr_ways, DCache_block_size))
+    val my_axi_bridge0 = Module(new AXIBridge(w, if(has_icache) (ICache_block_size * 8 / w) else 1 ))
+    val my_axi_bridge1 = Module(new AXIBridge(w, if(has_dcache) (DCache_block_size * 8 / w) else 1 ))
+    val my_mmc         = Module(new MemoryController(w, if(has_dcache) (DCache_block_size * 8 / w) else 1 ))
     val my_clint       = Module(new Clint(w))
                                         
     //IF stage
@@ -72,13 +67,25 @@ class MycpuCoreTop(w: Int, nr_mport: Int) extends Module with HasCoreTopConst{
     my_csr.io.out          <> my_wb.io.csr_out
     my_csr.io.clint_intr_t := my_clint.io.has_intr_t
     //Memory Access
-    //icache current ignored
-    my_if.io.inst_mem      <> my_icache.io.in
-    my_axi_bridge0.io.in   <> my_icache.io.out
+    //icache
+    if(has_icache){
+        val my_icache      = Module(new CacheTop(w, Cache_tag_width, ICache_nr_lines, 
+                                            ICache_nr_ways, ICache_block_size))
+        my_if.io.inst_mem      <> my_icache.io.in
+        my_axi_bridge0.io.in   <> my_icache.io.out
+    }else{
+        my_if.io.inst_mem      <> my_axi_bridge0.io.in
+    }
     io.axi_sram(0)         <> my_axi_bridge0.io.out
     //dcache
-    my_mem.io.data_mem     <> my_dcache.io.in
-    my_mmc.io.in           <> my_dcache.io.out
+    if(has_dcache){
+        val my_dcache      = Module(new CacheTop(w, Cache_tag_width, DCache_nr_lines,
+                                            DCache_nr_ways, DCache_block_size))
+        my_mem.io.data_mem     <> my_dcache.io.in
+        my_mmc.io.in           <> my_dcache.io.out
+    }else{
+        my_mem.io.data_mem     <> my_mmc.io.in
+    }
     my_clint.io.in         <> my_mmc.io.clint_out
     my_axi_bridge1.io.in   <> my_mmc.io.axi_out
     io.axi_sram(1)         <> my_axi_bridge1.io.out
