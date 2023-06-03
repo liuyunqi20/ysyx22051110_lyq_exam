06aa64f0b2766f860b3dbc73da7ecb724d72ab4f
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-03 13:31:16 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:31:16 up  2:21,  1 user,  load average: 0.33, 0.29, 0.21

--- npc/sim/cpp/src/cpu/cpu_exec.cpp
+++ npc/sim/cpp/src/cpu/cpu_exec.cpp
@@ -54,9 +54,9 @@ void wave_end(){
 
 void wave_dump(){
 #ifdef DUMPWAVE
-    if(g_nr_step > 2608750 && g_nr_step < 2608800)
+    if(g_nr_step > 2608520 && g_nr_step < 2608570)
         tfp->dump(contextp->time());
-    if(g_nr_step == 2608800)
+    if(g_nr_step == 2608570)
         wave_end();
 #endif
 }
