0e5e188e04c6a0c60eed1b797bd9b1a266d43ca1
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-06 21:26:05 +0800

    > sim RTL ysyx_22051110 陆韵岐 Linux llyyqq-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:05 up  2:07,  1 user,  load average: 0.02, 0.08, 0.02

---
+++ npc/sim/obj_dir/VSimTop__Syms.cpp
@@ -0,0 +1,34 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Symbol table implementation internals
+
+#include "VSimTop__Syms.h"
+#include "VSimTop.h"
+#include "VSimTop___024root.h"
+
+void VSimTop___024root____Vdpiexp_SimTop__DOT__my_core_top__DOT__my_wb__DOT__my_inst_monitor__DOT__catch_ebreak_TOP(VSimTop__Syms* __restrict vlSymsp, IData/*31:0*/ &i);
+
+// FUNCTIONS
+VSimTop__Syms::~VSimTop__Syms()
+{
+}
+
+VSimTop__Syms::VSimTop__Syms(VerilatedContext* contextp, const char* namep,VSimTop* modelp)
+    : VerilatedSyms{contextp}
+    // Setup internal state of the Syms class
+    , __Vm_modelp(modelp)
+    // Setup module instances
+    , TOP(namep)
+{
+    // Configure time unit / time precision
+    _vm_contextp__->timeunit(-12);
+    _vm_contextp__->timeprecision(-12);
+    // Setup each module's pointers to their submodules
+    // Setup each module's pointer back to symbol table (for public functions)
+    TOP.__Vconfigure(this, true);
+    // Setup scopes
+    __Vscope_SimTop__my_core_top__my_wb__my_inst_monitor.configure(this, name(), "SimTop.my_core_top.my_wb.my_inst_monitor", "my_inst_monitor", -12, VerilatedScope::SCOPE_OTHER);
+    // Setup export functions
+    for (int __Vfinal=0; __Vfinal<2; __Vfinal++) {
+        __Vscope_SimTop__my_core_top__my_wb__my_inst_monitor.exportInsert(__Vfinal, "catch_ebreak", (void*)(&VSimTop___024root____Vdpiexp_SimTop__DOT__my_core_top__DOT__my_wb__DOT__my_inst_monitor__DOT__catch_ebreak_TOP));
+    }
+}
